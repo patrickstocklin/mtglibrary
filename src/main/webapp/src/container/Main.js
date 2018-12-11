dd7193fe530eabdddff12b4cf62270cea9479fd5
patrickstocklin <patrickstocklin@gmail.com>
2018-12-10 19:26:13 -0500

    added form and button for search input

--- src/main/webapp/src/container/Main.js
+++ src/main/webapp/src/container/Main.js
@@ -7,35 +7,40 @@ class Main extends Component {
 
     constructor(props) {
       super(props);
-      this.cardName = "Arclight Phoenix";
-      this.state = {price: 0}
+      this.state = {
+        cardName: null,
+        price: null
+      }
 
-      this.ping = this.ping.bind(this);
+      this.getCardPrice = this.getCardPrice.bind(this);
     }
 
     render() {
       return (
         <div className="Main">
-          <header className="App-header">
-            <h1 className="App-title">Ping</h1>
-          </header>
-          <p className="App-intro">
-            <div>
-              <button onClick={this.ping}>Ping!</button>
-              <div>{this.cardName} : {this.state.price}</div>
-            </div>
-          </p>
+              <div className="search">
+                <input type="text" id="search" name="form" placeholder="Search..."/>
+                <input type="button" value="Search" id="mySearch" onClick={() => this.getCardPrice(
+                        document.getElementById("search").value
+                )}/>
+              </div>
+              <div>
+                {this.state.cardName} : {this.state.price}
+              </div>
         </div>
       );
     }
 
-    ping() {
-      axios.get("http://localhost:8080/Arclight+Phoenix/price").then(res => {
-        this.setState({price : res.data.usd});
-      }, err => {
-        alert("Server rejected response with: " + err);
-      });
+    getCardPrice(someName) {
+      var cardName = someName;
+      axios.get("http://localhost:8080/"+cardName+"/price").then(res => {
+              this.setState({price : "$"+res.data.usd});
+              this.setState({cardName : cardName});
+            }, err => {
+              alert("Server rejected response with: " + err);
+            });
     }
+
 }
 
 export default Main;
